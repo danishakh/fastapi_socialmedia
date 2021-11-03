2c4538701bb5f7d2f599707f8e762d429e49d770
danishakh <danishakh@gmail.com>
2021-11-03 01:15:00 -0700

    updated all routes to use ORM functions

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from . import models
 from .database import engine, get_db
 
@@ -15,12 +16,13 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
 # create class to keep a pydantic model of a Post and what it should have
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True  # when not sent in the body of req, it will default to True
-    rating: Optional[int] = None    # this will act as an optional field
+    
 
 
 # run until we get a successful connection
@@ -44,33 +46,37 @@ while True:
 def root():
     return {"message": "API Scene is ON!"}
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    return {"status":"success"}
+# test route
+# @app.get("/sqlalchemy")
+# def test_posts(db: Session = Depends(get_db)):
+#     return {"status":"success"}
 
 
+#  get all posts
 @app.get("/posts")
-def get_posts():
-    cursor.execute(""" SELECT * FROM posts """)
-    posts = cursor.fetchall()
-    print(posts)
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
     return {"data": posts}
 
 
+# add new post
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    print(new_post)
-    # commit the staged changes
-    conn.commit()
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    # new_post = models.Post(title=post.title, content=post.content, published=post.published)
+    # print(**post.dict())
+    new_post = models.Post(**post.dict())
+    # add to db
+    db.add(new_post)
+    db.commit()
+    # retrieve the new post we just created
+    db.refresh(new_post)
     return {"data": new_post}
 
 
+# get a post by id
 @app.get("/posts/{id}")
-def get_post(id: int, res: Response):
-    cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id),))
-    post = cursor.fetchone()
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         # res.status_code = status.HTTP_404_NOT_FOUND
         # return {"message": f"post with id: {id} was not found!"}
@@ -78,25 +84,34 @@ def get_post(id: int, res: Response):
     return {"data": post}
 
 
+# delete a post by id
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
 
-    if deleted_post == None:
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist!")
+
+    # synchronize_session=False is some default config, just got it from documentation
+    post.delete(synchronize_session=False)
+    db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
+# update a post by id
 @app.put("/posts/{id}")
-def updatePost(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
+def updatePost(id: int, post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    update_query = db.query(models.Post).filter(models.Post.id == id)
+    updated_post = update_query.first()
 
     if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist!")
 
-    return {"message": updated_post}
+    update_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+
+    return {"data": update_query.first()}
--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
+from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, column_property
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
 
 class Post(Base):
@@ -9,4 +12,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default='True', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()') ,nullable=False)
